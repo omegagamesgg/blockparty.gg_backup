0900a23233d5b143610ece92937fbc164c423607
Ron Forbes <ronforbes528@gmail.com>
2018-06-04 23:18:41 -0700

    Added initial express server + separated client

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Send back React's index.html by default
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Server listening on ${port}`);
