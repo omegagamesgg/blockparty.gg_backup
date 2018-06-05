491f1f3de4486b91eb670e47d14cbe6637bd432a
Ron Forbes <ronforbes528@gmail.com>
2018-06-05 00:49:24 -0700

    Added basic SocktIO server

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -12,6 +13,13 @@ app.get('*', (request, response) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port);
-
+server.listen(port);
 console.log(`Server listening on ${port}`);
+
+io.on('connect', function(socket) {
+  console.log(`connect: socket.id=${socket.id}`);
+
+  socket.on('disconnect', function(reason) {
+    console.log(`disconnect: socket.id=${socket.id}, reason=${reason}`);
+  });
+});
