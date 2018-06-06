788051257ca7b4680d98c2b95ec2e718aaf14d69
Ron Forbes <ronforbes528@gmail.com>
2018-06-05 21:23:32 -0700

    Parking Unity development to try again w/ React

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const port = process.env.PORT || 5000;
+
+var players = [];
+var nextPlayerId = 1;
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -12,12 +17,33 @@ app.get('*', (request, response) => {
   response.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
 server.listen(port);
 console.log(`Server listening on ${port}`);
 
 io.on('connect', function(socket) {
   console.log(`connect: socket.id=${socket.id}`);
+  players[socket.id] = "";
+
+  socket.on('rename player', function(data) {
+    var oldName = players[socket.id];
+    if(players.filter(name => name === data.Name).length > 0) {
+      console.log(`player renamed: { result: fail, oldName: ${oldName}, newName: ${newName} }`);
+      socket.emit('player renamed', {
+        result: 'fail',
+        oldName: oldName,
+        newName: oldName
+      });
+    }
+    else {
+      players[socket.id] = data.Name;
+      console.log(`player renamed: { result: success, oldName: ${oldName}, newName: ${players[socket.id]} }`);
+      io.emit('player renamed', {
+        result: 'success',
+        oldName: oldName,
+        newName: players[socket.id]
+      });
+    }
+  });
 
   socket.on('disconnect', function(reason) {
     console.log(`disconnect: socket.id=${socket.id}, reason=${reason}`);
