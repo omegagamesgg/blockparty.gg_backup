854b5eabf930b84f48737080f769589777193d53
Ron Forbes <ronforbes528@gmail.com>
2018-06-25 12:31:42 -0700

    Started prototype game (Clicky Race) UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main className="App">
+        <h1 className="App-instructions">Click as many times as possible in 2 minutes!</h1>
+        <div className="App-score-group">
+          <p className="App-score-label">Score</p>
+          <h2 className="App-score-text">0</h2>
+        </div>
+      </main>
     );
   }
 }
