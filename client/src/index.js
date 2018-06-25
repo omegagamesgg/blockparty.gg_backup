157495bbfe02d9bec33b583f9a2cd52936bd827e
Ron Forbes <ronforbes528@gmail.com>
2018-06-25 12:00:46 -0700

    Backed up the old site and started from a fresh React/Firebase boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import * as firebase from 'firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
 registerServiceWorker();
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD5fiqWtwdnuqGeGM5RFodALEP-PFyCI9o",
+  authDomain: "block-party-31d52.firebaseapp.com",
+  databaseURL: "https://block-party-31d52.firebaseio.com",
+  projectId: "block-party-31d52",
+  storageBucket: "block-party-31d52.appspot.com",
+  messagingSenderId: "235246870848"
+};
+firebase.initializeApp(config);
