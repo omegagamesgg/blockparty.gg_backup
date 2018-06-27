2190bc4ae1c3c7c63d9229e6bd576c871d3adc39
Ron Forbes <ronforbes528@gmail.com>
2018-06-27 11:45:59 -0700

    Added Landing Page and mailing list sign-up

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -3,8 +3,24 @@ import './LandingPage.css';
 
 const LandingPage = () =>
   <div className="LandingPage">
-    <h1>Block Party</h1>
-    <p>Coming soon</p>
+    <img src="images/BlockPartyLogo.png" alt="Block Party"></img>
+    <p className="LandingPage-tagline">Live social minigame network<br /><br />Coming soon</p>
+    <div className="LandingPage-signUpForm" id="mc_embed_signup">
+      <form action="https://blockparty.us18.list-manage.com/subscribe/post?u=063934cb27760e2adccfeab1b&amp;id=96738a899b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+        <div id="mc_embed_signup_scroll">
+	        <p className="LandingPage-callToAction">Sign up for email updates</p>
+          <div className="mc-field-group">
+            <input type="email" name="EMAIL" placeholder="Email address" className="required email LandingPage-inputField" id="mce-EMAIL" />
+          </div>
+	        <div id="mce-responses" className="clear">
+		        <div className="response" id="mce-error-response" style={{display:'none'}}></div>
+		        <div className="response" id="mce-success-response" style={{display:'none'}}></div>
+	        </div>
+          <div style={{position: 'absolute', left: -5000 + 'px'}} aria-hidden="true"><input type="text" name="b_063934cb27760e2adccfeab1b_96738a899b" tabIndex="-1" value="" /></div>
+          <div className="clear"><input type="submit" value="Sign up" name="subscribe" id="mc-embedded-subscribe" className="LandingPage-submitButton button" /></div>
+        </div>
+      </form>
+    </div>
   </div>
 
 export default LandingPage;
